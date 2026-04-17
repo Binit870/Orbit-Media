2bfa9cf72978b71b6fd7ece13caa8621ef23cb0a
Binit870 <binitpatra870@gmail.com>
2026-04-17 22:53:31 +0530

    folder structure

--- om-frontend/src/App.jsx
+++ om-frontend/src/App.jsx
@@ -1,7 +1,12 @@
-export default function App() {
+import react from 'react';
+import ContentFlywheel from './ContentFlywheel';
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="App">
+      <ContentFlywheel />
+    </div>
+  );
 }
+
+export default App;
