fb442ef97abbc9040df088914aa1a6d9e2997851
ADITYA SINGH <adityasingh80026@gmail.com>
2026-04-18 12:37:21 +0530

    top 5 sections except fly wheel

--- om-frontend/src/App.jsx
+++ om-frontend/src/App.jsx
@@ -1,10 +1,19 @@
-import react from 'react';
+import React from 'react';
 import ContentFlywheel from './ContentFlywheel';
-
+import Hero from './components/sections/Hero';
+import TrustedBy from './components/sections/TrustedBy';
+import Stats from './components/sections/Stats';
+import ShortFormSection from './components/sections/ShortFormSection';
+import ViralAndTrailersSections from './components/sections/Viralandtrailerssections';
 function App() {
   return (
     <div className="App">
       <ContentFlywheel />
+      <Hero />
+      <TrustedBy/>
+      <Stats/>
+      <ShortFormSection/>
+      <ViralAndTrailersSections/>
     </div>
   );
 }
